e53ddc8b353cd80e21f27c1a2a4976b4da38fc9d
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-24 10:53:46 +0100

    Updated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,51 @@
 import "./App.css";
-import { Text } from "@chakra-ui/react";
-import AuthLayout from "./components/auth-layout/auth-layout";
+import { ChakraProvider, CSSReset, theme } from "@chakra-ui/react";
+import { User } from "./redux/user/user.types";
+import { bindActionCreators, Dispatch } from "redux";
+import { setCurrentUser } from "./redux/user/user.actions";
+import { createStructuredSelector } from "reselect";
+import { AppStateTypes } from "./redux/root.reducer";
+import { selectCurrentUser } from "./redux/user/user.selector";
+import { connect } from "react-redux";
+import { customTheme } from "./styles/theme";
+import { Route, Switch } from "react-router-dom";
+import { BaseRoutes } from "./routes/route.component";
+import ProtectedRoute from "./routes/protected-route.component";
+import UnProtectedRoute from "./routes/unprotected-route.component";
 
 const App = () => {
   return (
-    <AuthLayout>
-      <Text>Something</Text>
-    </AuthLayout>
+    <ChakraProvider theme={{ ...theme, ...customTheme }}>
+      <CSSReset />
+      <Switch>
+        {BaseRoutes.map(({ isProtected, isUnProtected, ...otherProps }) => {
+          if (isProtected) return <ProtectedRoute {...otherProps} />;
+
+          if (isUnProtected) return <UnProtectedRoute {...otherProps} />;
+
+          return <Route {...otherProps} />;
+        })}
+      </Switch>
+    </ChakraProvider>
   );
 };
 
-export default App;
+type LinkDispatchProps = {
+  setCurrentUser: (user: User) => void;
+};
+
+type LinkStateProps = {
+  currentUser: User;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): LinkDispatchProps => ({
+  setCurrentUser: bindActionCreators(setCurrentUser, dispatch),
+});
+
+const mapStateToProps = createStructuredSelector<AppStateTypes, LinkStateProps>(
+  {
+    currentUser: selectCurrentUser,
+  }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
