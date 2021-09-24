11ee256fea026f272241c3097d708b712f83ec7c
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-24 10:49:50 +0100

    Created redux root reducer

---
+++ src/redux/root.reducer.ts
@@ -0,0 +1,29 @@
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import sessionStorage from "redux-persist/lib/storage/session";
+
+import { ROOT_REDUX_PERSIST_KEY } from "../utils/utils";
+import userReducer from "./user/user.reducer";
+
+const persistConfig = {
+  key: ROOT_REDUX_PERSIST_KEY,
+  storage: sessionStorage,
+  whitelist: [
+    "bookReducer",
+    "cartReducer",
+    "userReducer",
+    "trainingReducer",
+    "purchaseReducer",
+    "eventReducer",
+  ],
+};
+
+const rootReducer = combineReducers({
+  userReducer,
+});
+
+export type AppStateTypes = ReturnType<typeof rootReducer>;
+
+export default rootReducer;
+
+export const persistedReducer = persistReducer(persistConfig, rootReducer);
