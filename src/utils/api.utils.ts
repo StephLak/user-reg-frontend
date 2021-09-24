ae6c41d71a6db88bfa84505ca3e351abf8280793
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-24 10:50:56 +0100

    Added app utils

---
+++ src/utils/api.utils.ts
@@ -0,0 +1,145 @@
+import { makeRequest, RequestOptions } from "react-query-pro";
+import { createStandaloneToast } from "@chakra-ui/react";
+import { getItemFromStorage, USER_TOKEN_KEY } from "./utils";
+
+const toast = createStandaloneToast();
+
+export type ErrorType = {
+  status?: number;
+  message: string;
+} | null;
+
+export let BASE_URL = "https://reqres.in/api";
+
+// if (process.env.NODE_ENV === "production") {
+//   BASE_URL = "https://reqres.in/api";
+// }
+
+export type HandleRequestOptions = { token?: string } & RequestOptions;
+
+export type HeaderOptions = { token?: string } & any;
+
+export const getHeaders = (headerOptions?: HeaderOptions) => {
+  const { token, ...otherOptions } = headerOptions || {};
+
+  return {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      ...(token && { "X-Auth-Token": `${token}` }),
+      ...otherOptions,
+    },
+  };
+};
+
+export const handleRequest = async (options: HandleRequestOptions) => {
+  try {
+    const { token, data, url } = options;
+
+    const axiosConfig = {
+      url: `${url}`,
+      headers: getHeaders({ token }),
+      ...(data && { data }),
+      ...options,
+    };
+
+    const response = await makeRequest(axiosConfig);
+
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const IS_ONLINE = navigator.onLine;
+
+export const formatFormSubmissionErrors = (responseErrors: any) => {
+  if (!responseErrors) return;
+
+  const errorsKeys = Object.keys(responseErrors);
+
+  const errors = errorsKeys
+    ?.map((key) => responseErrors[key])
+    ?.filter((error) => error)
+    ?.flat(Infinity);
+
+  return errors;
+};
+
+export const handleNetworkErrors = ({
+  networkErrorMessage,
+  error,
+  validationErrors,
+  errorMessage = "Sorry we encountered an unexpected error while processing this request, please try again",
+  isFetch = false,
+}: {
+  networkErrorMessage?: string;
+  error: any;
+  validationErrors?: any;
+  errorMessage?: any;
+  isFetch?: boolean;
+}) => {
+  if (!error) return;
+
+  console.log({ error, validationErrors, networkErrorMessage });
+
+  if (validationErrors && typeof validationErrors === "object")
+    return formatFormSubmissionErrors(validationErrors);
+
+  const { response, request } = error || {};
+
+  if (networkErrorMessage) return [networkErrorMessage];
+  else if (!IS_ONLINE)
+    return [
+      "You are currently offline. Please, confirm your internet connection and try again",
+    ];
+  else if (request && !response)
+    return [
+      `Sorry, an unexpected error while ${
+        isFetch ? "fetching the data you requested" : "processing your request "
+      }, please try again`,
+    ];
+  else if (!IS_ONLINE && (!request || !response))
+    return [
+      "An unexpected error occurred, confirm your internet connection and please try agian",
+    ];
+  else return [errorMessage];
+};
+
+export const saveToStorage = (
+  itemKey: number,
+  storage: any,
+  itemToSave: object
+) => storage.setItem(itemKey, itemToSave);
+
+export const getFromStorage = (itemKey: number, storage: any) =>
+  storage.getItem(itemKey) || "";
+
+export const deleteFromStorage = (itemKey: number, storage: any) =>
+  storage.removeItem(itemKey);
+
+export const axiosHeader = () => {
+  const token = getItemFromStorage({
+    storage: localStorage,
+    itemKey: USER_TOKEN_KEY,
+  });
+  const headers = getHeaders({ token });
+
+  return headers;
+};
+
+export const axiosRequestError = (err: any) => {
+  const { errors, data, message } = err?.response?.data || {};
+
+  const formattedErrors = handleNetworkErrors({
+    error: err,
+    validationErrors: errors || data,
+    networkErrorMessage: message,
+  });
+  if (formattedErrors)
+    toast({
+      status: "error",
+      title: "Error",
+      description: formattedErrors && formattedErrors[0],
+    });
+};
