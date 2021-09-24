8bab333f492a2d947f802e4cbd21f3bfd85c4030
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-24 10:48:40 +0100

    Created user redux type

---
+++ src/redux/user/user.types.ts
@@ -0,0 +1,17 @@
+export enum UserActionTypesEnums {
+  SET_CURRENT_USER = "SET_CURRENT_USER",
+}
+
+export interface IUser {
+  id: number;
+  token: string;
+}
+
+export type User = IUser | null;
+
+export type SetCurrentUser = {
+  type: UserActionTypesEnums.SET_CURRENT_USER;
+  payload: User;
+};
+
+export type UserActionsTypes = SetCurrentUser;
